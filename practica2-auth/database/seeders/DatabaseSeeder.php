3020634bd298cc0daa7e56803716c70e3bfa2cf2
Alejandro Avalos Espinosa <2316502300@uptex.edu.mx>
2026-06-25 19:13:12 -0600

    Practica 3: pruebas de APIs con Postman

--- practica2-auth/database/seeders/DatabaseSeeder.php
+++ practica2-auth/database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            PostmanUserSeeder::class,
             CategoriaSeeder::class,
             ProductoSeeder::class,
         ]);
